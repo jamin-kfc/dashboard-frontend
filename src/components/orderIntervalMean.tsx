4bff7276499138790ea2a3c1db50dcd72ff0d7b7
it intern <itintern@koreafoods.co.uk>
2025-08-06 11:14:42 +0100

    feat(statsBoard): show stat for clients

--- src/components/orderIntervalMean.tsx
+++ src/components/orderIntervalMean.tsx
@@ -1,4 +1,4 @@
-// Add imports for the card. Test the card mean. Put it together with the order history bar chart and serve.
+// Add imports for the card. Test the card stat. Put it together with the order history bar chart and serve.
 // use the port forwarding method https://github.com/microsoft/WSL/issues/4150
 // Possibly this https://ngrok.com/docs
 
@@ -7,21 +7,46 @@ import { Card, CardTitle, CardContent, CardHeader } from "./ui/card";
 import { baseUrl } from "../EnvVars.tsx"
 
 interface OrderIntervalProps {
-    cardCode: string
+  cardCode: string
 }
 
-interface OrderMeanInterval {
-  mean: string
+interface CardStatistic {
+  stat: string
 }
+
+interface QuarterStatistic {
+  quarter: string
+  quarterlySales: string
+}
+
 
 export function OrderIntervalMean({cardCode}: OrderIntervalProps) {
 
 
-  const URL = baseUrl + `api/orders/${cardCode}/mean/`;
-  const [orderIntervalMean, setOrderIntervalMean] = useState<OrderMeanInterval>({mean: 'No mean recevied.'});
-  
-  const fetchData = async () => {
-    fetch(URL)
+  const statURL = baseUrl + `api/orders/${cardCode}/mean/`;
+  const weekURL = baseUrl + `api/orders/${cardCode}/byWeek/`;
+  const monthURL = baseUrl + `api/orders/${cardCode}/byMonth/`;
+  const quarterURL = baseUrl + `api/orders/${cardCode}/byQuarter/`;
+  const [orderIntervalMean, setOrderIntervalMean] = useState<CardStatistic>({stat: 'No stat recevied.'});
+  const [salesByWeek, setSalesByWeek] = useState<CardStatistic>({stat: 'No stat recevied.'});
+  const [salesByMonth, setSalesByMonth] = useState<CardStatistic>({stat: 'No stat recevied.'});
+  const [salesByQuarter, setSalesByQuarter] = useState<QuarterStatistic>({quarter: 'No stat recevied.', quarterlySales: 'No stat received'});
+
+  const fetchMean = async () => {
+   fetch(statURL)
+   .then(response => {
+     if (!response.ok) {
+       throw new Error(`Response status: ${response.status}`);
+     } return response;
+   })
+   .then(response => response.json())
+   .then(data => {
+     setOrderIntervalMean(data[0]);
+   });
+  };
+
+  const fetchWeekSales = async () => {
+    fetch(weekURL)
     .then(response => {
       if (!response.ok) {
         throw new Error(`Response status: ${response.status}`);
@@ -29,12 +54,42 @@ export function OrderIntervalMean({cardCode}: OrderIntervalProps) {
     })
     .then(response => response.json())
     .then(data => {
-      setOrderIntervalMean(data[0]);
+      setSalesByWeek(data[0]);
     })
   };
 
+  const fetchMonthSales = async () => {
+    fetch(monthURL)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      } return response;
+    })
+    .then(response => response.json())
+    .then(data => {
+      setSalesByMonth(data[0]);
+    })
+  };
+
+  const fetchQuarterData = async () => {
+    fetch(quarterURL)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      } return response;
+    })
+    .then(response => response.json())
+    .then(data => {
+      setSalesByQuarter(data[0]);
+    })
+  };
+
+
   useEffect(() => {
-    fetchData();
+    fetchMean();   
+    fetchWeekSales();
+    fetchMonthSales();
+    fetchQuarterData();
   }, [cardCode])
 
   return (
@@ -44,9 +99,38 @@ export function OrderIntervalMean({cardCode}: OrderIntervalProps) {
                 <CardTitle> Average Order Interval </CardTitle>
             </CardHeader>
             <CardContent className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
-                {orderIntervalMean.mean} Days
+                {orderIntervalMean.stat} Days
             </CardContent>
         </Card>
+	
+        <Card className="w-full max-w-sm">
+          <CardHeader>
+              <CardTitle> Weekly Sales </CardTitle>
+          </CardHeader>
+          <CardContent className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
+              {salesByWeek.stat} 
+          </CardContent>
+        </Card>
+
+      <Card className="w-full max-w-sm">
+        <CardHeader>
+            <CardTitle> Monthly Sales </CardTitle>
+        </CardHeader>
+        <CardContent className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
+            {salesByMonth.stat} 
+        </CardContent>
+      </Card>
+	
+      <Card className="w-full max-w-sm">
+        <CardHeader>
+            <CardTitle> Quarterly Sales </CardTitle>
+        </CardHeader>
+        <CardContent className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
+            {salesByQuarter.quarter}
+            {salesByQuarter.quarterlySales} 
+        </CardContent>
+      </Card>
+
     </>
   )
 }
